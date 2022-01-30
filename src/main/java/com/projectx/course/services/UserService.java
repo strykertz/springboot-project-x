0459af657139f0ba4ca4e13234cb55808163ad02
Marcus Franco <marcusvgf16@gmail.com>
2022-01-30 15:34:21 -0300

    DatabaseException

--- src/main/java/com/projectx/course/services/UserService.java
+++ src/main/java/com/projectx/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projectx.course.entities.User;
 import com.projectx.course.repositories.UserRepository;
+import com.projectx.course.services.exceptions.DataBaseException;
 import com.projectx.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		 repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
